1cf2550731fa002d18fe74769a68bffcbeafaa94
Hebron George <hebrontgeorge@gmail.com>
2021-11-26 15:35:25 -0500

    Introduce a Presenter that's responsible for formatting the quote for discord

--- quotes_service.rb
+++ quotes_service.rb
@@ -1,5 +1,7 @@
 require 'faraday'
 require 'json'
+require 'tzinfo'
+require 'date'
 
 module QuotesService
   def self.add_quote(data)
@@ -17,12 +19,44 @@ module QuotesService
   end
 
   def self.find_quote(keyword)
-    JSON.parse(client.post('find-quote') { |req| req.params = {keyword: keyword} }.body)
+    response = client.post('find-quote') { |req| req.params = {keyword: keyword} }
+    Presenter.new(response).formatted_quote
   end
 
   def self.client
     url     = ENV['QUOTES_BACKEND_API']
     headers = {'Content-Type' => 'application/json'}
     Faraday.new(url: url, headers: headers)
+  end
+end
+
+class Presenter
+  attr_reader :message
+  def initialize(quote_response)
+    @message = quote_response.success? ? JSON.parse(quote_response.body).dig('data') : empty_quote
+  end
+
+  def formatted_quote
+    "> #{message['message']}\n"\
+      "Added By: `#{message['author']}` | Saved At: `#{saved_at}` | Channel: `#{message['channel']}`"
+  end
+
+  private
+
+  def empty_quote
+    {
+      'data': {
+        'author':   'N/A',
+        'saved_at': 'N/A',
+        'channel':  'N/A',
+        'message':  'No quotes found.',
+      }
+    }
+  end
+
+  def saved_at
+    tz = TZInfo::Timezone.get('America/New_York')
+    s = message['saved_at']
+    s == 'N/A' ? 'N/A' : tz.to_local(DateTime.parse(s)).strftime('%Y-%m-%d %H:%M:%S %p %Z')
   end
 end
